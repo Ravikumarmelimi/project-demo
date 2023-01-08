ddf8697d01a3ab9d3b5a289476ec9387be1d0a61
RaviM <ravikumar.melimi@gmail.com>
2023-01-08 20:48:34 +0530

    pset3 change to uppercase of userid

---
+++ pset3.py
@@ -0,0 +1,8 @@
+# PSET3
+# Write a python script that captures USN of a student from std. input 
+# and converts all lowercase letters
+# to uppercase and prints the same.
+
+usn = input("Enter the username ID of the Student ")
+usn_upper = usn.upper()
+print(usn_upper)
