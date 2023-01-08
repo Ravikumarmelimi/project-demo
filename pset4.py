11e1720a2087027dc76b34105ce5d13b48c9160f
RaviM <ravikumar.melimi@gmail.com>
2023-01-08 21:01:27 +0530

    pset4 to print substring is added

---
+++ pset4.py
@@ -0,0 +1,12 @@
+# PSET4
+# In the string hakuna matata 
+# print the substring tata using slicing. 
+# Try with both positive and negative indices
+
+str1 = "hakuna matata"
+x = len(str1)
+sub_str1 = str1[x-4:x]  # positive indices
+sub_str2 = str1[-4:]
+print(x)
+print(sub_str1)
+print(sub_str2)
