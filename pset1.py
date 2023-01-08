17d2d06d5032cb37d299039ae1f169f3e693878b
RaviM <ravikumar.melimi@gmail.com>
2023-01-08 19:39:04 +0530

    adding pset1 to read input and display output

---
+++ pset1.py
@@ -0,0 +1,6 @@
+# PSET1: 
+# python script that reads an input from the std.input
+# and prints the same in the std output
+x = input("Enter an Even Number ")
+print("The entered Even number is {}".format(x))
+
